b617a98e7fa7646d8c50e022f202b52bc1dd3770
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-23 13:00:45 +0200

    Update MazesMod.java

--- src/main/java/com/example/examplemod/MazesMod.java
+++ src/main/java/com/example/examplemod/MazesMod.java
@@ -1,20 +1,50 @@
 package com.example.examplemod;
 
+import static net.minecraft.core.registries.Registries.DIMENSION;
+import static net.minecraft.core.registries.Registries.DIMENSION_TYPE;
+
+import java.util.OptionalLong;
+import java.util.function.Function;
+
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.coordinates.Vec3Argument;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.ClickEvent.Action;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.commands.TeleportCommand;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
+import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.ITeleporter;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedOutEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -33,27 +63,18 @@ import org.slf4j.Logger;
 @Mod(MazesMod.MODID)
 public class MazesMod {
 
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "mazesmod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    //
+    public static final ResourceKey<Level> MAZES_LEVEL = ResourceKey.create(DIMENSION, new ResourceLocation(MODID, "mazes"));
 
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
     public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
     public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
-
-    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
     public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
             .alwaysEat().nutrition(1).saturationMod(2f).build())));
-
-    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
     public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
@@ -63,61 +84,112 @@ public class MazesMod {
 
     public MazesMod(){
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so blocks get registered
         BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
         CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event){
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+       //
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event){
         if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
             event.accept(EXAMPLE_BLOCK_ITEM);
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event){
-        // Do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents{
+    public static class ClientModEvents {
+
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event){
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+            //
         }
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID)
+    public static class Events {
+
+        @SubscribeEvent
+        public static void onPlayerJoin(PlayerLoggedInEvent event){
+            if(event.getEntity().level().isClientSide) return;
+            //
+        }
+
+        @SubscribeEvent
+        public static void onPlayerLeave(PlayerLoggedOutEvent event){
+            if(event.getEntity().level().isClientSide) return;
+            //
+        }
+
+        @SubscribeEvent
+        public static void onCmdReg(RegisterCommandsEvent event){
+            event.getDispatcher().register(Commands.literal("mz-reg").requires(con -> con.getPlayer() != null)
+                .then(Commands.argument("id", StringArgumentType.word())
+                    .then(Commands.argument("start", Vec3Argument.vec3())
+                        .then(Commands.argument("end", Vec3Argument.vec3())
+                .executes(context ->{
+                    try{
+                        MazeManager.register(context, false);
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                        context.getSource().sendFailure(Component.literal("Error during command execution. Check log for details."));
+                    }
+                    return 0;
+                }
+            )))));
+            event.getDispatcher().register(Commands.literal("mz-upd").requires(con -> con.getPlayer() != null)
+                .then(Commands.argument("id", StringArgumentType.word())
+                    .then(Commands.argument("start", Vec3Argument.vec3())
+                        .then(Commands.argument("end", Vec3Argument.vec3())
+                .executes(context ->{
+                    try{
+                        MazeManager.register(context, true);
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                        context.getSource().sendFailure(Component.literal("Error during command execution. Check log for details."));
+                    }
+                    return 0;
+                }
+            )))));
+            event.getDispatcher().register(Commands.literal("mz-del").requires(con -> con.getPlayer() != null)
+                .then(Commands.argument("id", StringArgumentType.word())
+                .executes(context ->{
+
+                    return 0;
+                }
+            )));
+            event.getDispatcher().register(Commands.literal("mz-tpdim").executes(context ->{
+                try{
+                    context.getSource().getPlayer().changeDimension(context.getSource().getServer().getLevel(MAZES_LEVEL), new ITeleporter() {
+                        @Override
+                        public Entity placeEntity(Entity entity, ServerLevel currentWorld, ServerLevel destWorld, float yaw, Function<Boolean, Entity> repositionEntity) {
+                            return ITeleporter.super.placeEntity(entity, currentWorld, destWorld, yaw, repositionEntity);
+                        }
+                        @Override
+                        public boolean isVanilla(){
+                            return false;
+                        }
+                    });
+                }
+                catch(Throwable e){
+                    e.printStackTrace();
+                }
+                return 0;
+            }));
+        }
+
     }
 
 }

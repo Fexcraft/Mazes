07e0fdd0006c4a11e4f5a6daa0d589dd7bdd0d41
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-19 16:40:33 +0200

    Update MazeInst.java

--- src/main/java/com/example/examplemod/MazeInst.java
+++ src/main/java/com/example/examplemod/MazeInst.java
@@ -2,6 +2,9 @@ package com.example.examplemod;
 
 import java.util.ArrayList;
 
+import net.fexcraft.app.json.JsonArray;
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.Time;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.ChunkPos;
 
@@ -16,6 +19,23 @@ public class MazeInst {
 		root = maze;
 		start = s;
 		end = e;
+	}
+
+	public MazeInst(JsonMap map){
+		root = MazeManager.MAZES.get(map.get("root").string_value());
+		JsonArray arr = map.getArray("start");
+		start = new ChunkPos(arr.get(0).integer_value(), arr.get(1).integer_value());
+		arr = map.getArray("end");
+		end = new ChunkPos(arr.get(0).integer_value(), arr.get(1).integer_value());
+	}
+
+	public JsonMap save(){
+		JsonMap map = new JsonMap();
+		map.add("saved", Time.getAsString(Time.getDate()));
+		map.add("root", root.id);
+		map.add("start", new JsonArray(start.x, start.z));
+		map.add("end", new JsonArray(end.x, end.z));
+		return map;
 	}
 
 }

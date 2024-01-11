732377a3e6c9107b462c69de309b4c2e70d95855
Ferdinand Calo <fexcalo@gmail.com>
2024-01-11 10:52:05 +0100

    Update Maze.java

--- src/main/java/net/fexcraft/mod/mazemod/Maze.java
+++ src/main/java/net/fexcraft/mod/mazemod/Maze.java
@@ -5,11 +5,15 @@ import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.Time;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.fml.loading.FMLPaths;
 
 import java.io.File;
 import java.util.ArrayList;
+
+import static net.minecraft.core.registries.Registries.DIMENSION;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -21,12 +25,12 @@ public class Maze {
 	public BlockPos exit;
 	public final String id;
 	public final ArrayList<BlockPos> chests = new ArrayList<>();
-	public ResourceLocation dimid;
+	public ResourceKey<Level> dimid;
 	public BlockPos orgpos;
 	public BlockPos tppos;
 	//
-	public int instances;
-	public int cooldown;
+	public int instances = 1;
+	public int cooldown = 60;
 
 	public Maze(String nid){
 		id = nid;
@@ -54,7 +58,7 @@ public class Maze {
 			array = map.getArray("tp_pos");
 			orgpos = new BlockPos(array.get(0).integer_value(), array.get(1).integer_value(), array.get(2).integer_value());
 		}
-		dimid = new ResourceLocation(map.getString("dim_id", null));
+		dimid = ResourceKey.create(DIMENSION, new ResourceLocation(map.getString("dim_id", null)));
 		if(map.has("chests")){
 			array = map.getArray("chests");
 			array.value.forEach(val -> {
@@ -90,7 +94,7 @@ public class Maze {
 			map.add("tp_pos", new JsonArray(tppos.getX(), tppos.getY(), tppos.getZ()));
 		}
 		if(dimid != null){
-			map.add("dim_id", dimid.toString());
+			map.add("dim_id", dimid.location().toString());
 		}
 		if(chests.size() > 0){
 			JsonArray array = new JsonArray();

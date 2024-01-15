3a527c19e666987b3d386435405cc8a8abae3b6d
Ferdinand Calo <fexcalo@gmail.com>
2024-01-15 23:00:22 +0100

    Update EnrxitBlock.java

--- src/main/java/net/fexcraft/mod/mazemod/EnrxitBlock.java
+++ src/main/java/net/fexcraft/mod/mazemod/EnrxitBlock.java
@@ -13,12 +13,14 @@ import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.Vec3;
 
+import java.util.ArrayList;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class EnrxitBlock extends Block {
 
-	private boolean exit;
+	public final boolean exit;
 
 	public EnrxitBlock(boolean isexit){
 		super(BlockBehaviour.Properties.of()
@@ -34,7 +36,34 @@ public class EnrxitBlock extends Block {
 	public void entityInside(BlockState state, Level world, BlockPos pos, Entity entity){
 		if(entity instanceof Player == false) return;
 		Player player = (Player)entity;
-		player.sendSystemMessage(Component.literal(pos.toShortString()));
+		PlayerData data = MazeManager.getPlayerData(player);
+		if(data.cooldown > 0){
+			data.cooldown--;
+			return;
+		}
+		if(world.dimension().equals(MazesMod.MAZES_LEVEL)){
+			if(exit) data.teleport(player);
+		}
+		else{
+			Maze maze = MazeManager.getMazeFor(pos, exit);
+			if(maze == null){
+				data.last = new BlockPos(pos);
+				data.lexit = exit;
+				player.sendSystemMessage(Component.literal("Gate not linked. (" + pos.toShortString() + ")"));
+				player.sendSystemMessage(Component.literal("Use '/mz link <id>' to link to a template."));
+			}
+			else if(!exit){
+				MazeInst inst = MazeManager.getFreeInst(player, maze);
+				if(inst == null){
+					player.sendSystemMessage(Component.literal("All instances are occupied."));
+				}
+				else{
+					data.teleport(player, inst);
+					ArrayList<Player> party = Parties.PARTIES.get(player.getGameProfile().getId());
+				}
+			}
+		}
+		data.cooldown += 100;
 	}
 
 

8db9c0f73a45577bc986220481a6049ad2e3b1a6
Ferdinand Calo <fexcalo@gmail.com>
2024-01-15 23:00:34 +0100

    Create SelectionCache.java

---
+++ src/main/java/net/fexcraft/mod/mazemod/SelectionCache.java
@@ -0,0 +1,13 @@
+package net.fexcraft.mod.mazemod;
+
+import net.minecraft.core.BlockPos;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class SelectionCache {
+
+    public BlockPos first;
+    public BlockPos second;
+
+}

7f39a9e24e8e770d545fdc9f60e982adb982c311
Ferdinand Calo <fexcalo@gmail.com>
2024-01-16 01:53:26 +0100

    Update PlayerData.java

--- src/main/java/net/fexcraft/mod/mazemod/PlayerData.java
+++ src/main/java/net/fexcraft/mod/mazemod/PlayerData.java
@@ -28,19 +28,9 @@ public class PlayerData {
 		try{
 			ServerLevel lvl = ServerLifecycleHooks.getCurrentServer().getLevel(MAZES_LEVEL);
 			player.moveTo(to.zeropos.offset(to.root.entry.get(0)).getCenter());
-			player.changeDimension(lvl, new ITeleporter() {
-				@Override
-				public Entity placeEntity(Entity entity, ServerLevel currentWorld, ServerLevel destWorld, float yaw, Function<Boolean, Entity> repositionEntity){
-					entity = ITeleporter.super.placeEntity(entity, currentWorld, destWorld, yaw, repositionEntity);
-					instin = to;;
-					to.players.add((Player)entity);
-					return entity;
-				}
-				@Override
-				public boolean isVanilla(){
-					return false;
-				}
-			});
+			instin = to;
+			Entity entity = player.changeDimension(lvl, new ITeleporter(){});
+			if(entity != null) instin.players.add((Player)entity);
 		}
 		catch(Throwable e){
 			e.printStackTrace();
@@ -49,7 +39,7 @@ public class PlayerData {
 
 	public void teleport(Player player){
 		try{
-			ServerLevel lvl = player.getServer().getLevel(OVERWORLD);
+			ServerLevel lvl = ServerLifecycleHooks.getCurrentServer().overworld();
 			BlockPos pos = instin.root.gate_out == null ? instin.root.gate_in : instin.root.gate_out;
 			instin.players.remove(player);
 			player.moveTo(pos.getCenter());
